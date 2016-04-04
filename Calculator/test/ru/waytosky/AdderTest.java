a068e28587a48d7dff923e3944b679bd93ec6a89
ajratb <ajratb@ya.ru>
2016-04-04 21:53:41 +0700

    evening commit3

---
+++ Calculator/test/ru/waytosky/AdderTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.waytosky;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ayrat
+ */
+public class AdderTest {
+    
+    public AdderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Adder.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 5;
+        int b = 9;
+        Adder instance = new Adder();
+        int expResult = 14;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
