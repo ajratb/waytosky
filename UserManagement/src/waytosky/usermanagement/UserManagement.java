fed23aadeac12f0eb113983d431ee4a07846edcb
ayrat <ayrat@nok2-test1.npopm.ru>
2016-05-13 18:07:34 +0700

    changed design(fxml) and set connection

--- UserManagement/src/waytosky/usermanagement/UserManagement.java
+++ UserManagement/src/waytosky/usermanagement/UserManagement.java
@@ -8,10 +8,12 @@ package waytosky.usermanagement;
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /**
@@ -19,20 +21,36 @@ import javafx.stage.Stage;
  * @author ayrat
  */
 public class UserManagement extends Application {
-    
+
+    public static Connection conn;
+
     @Override
     public void start(Stage stage) throws Exception {
-        
+
         //Создаем подключение
-        DriverManager.registerDriver(new SQLServerDriver());
-            Connection conn = DriverManager.getConnection(url, "", "");
-        
-        Parent root = FXMLLoader.load(getClass().getResource("usermanagement.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+        //если conn получить не удастся то выход из приложения
+        String url = "jdbc:sqlserver://saprtp\\smarteamdb;databaseName=javadb";
+        try {
+            DriverManager.registerDriver(new SQLServerDriver());
+            conn = DriverManager.getConnection(url, "javadb",
+                    "superstar");
+
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("usermanagement.fxml"));
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (SQLException sqlEx) {
+            System.out.println("ex");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Вход в БД не выполнен");
+            alert.setHeaderText("Не удалось установить связь с базой данных");
+
+            alert.setContentText(sqlEx.getMessage());
+            alert.showAndWait();
+        }
     }
 
     /**
@@ -41,5 +59,5 @@ public class UserManagement extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
